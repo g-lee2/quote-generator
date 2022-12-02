c04ce3df4eae598c8a19bc62d7c6ecb72bb83b05
Grace Lee <gracexsnoopy@gmail.com>
2022-12-02 14:48:46 -0800

    Make buttons functional and be able to tweet out quotes

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
 
-// Show New Quote 
+// Show New Local Quote 
 // function newQuote() {
-//   // Pick a random quote from apiQuotes array
 //   const quote = localQuotes[Math.floor(Math.Random() * apiQuotes.length)];
 //   console.log(quote);
 // }
 
 function newQuote() {
-  // Pick a random quote from apiQuotes array
-  const quote = apiQuotes[Math.floor(Math.Random() * apiQuotes.length)];
-  console.log(quote);
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  if (!quote.author) {
+    authorText.textContent = 'Unknown'
+  } else {
+    authorText.textContent = quote.author;
+  }
+  if (quote.text.length > 50) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  quoteText.textContent = quote.text;
 }
 
-//Get Quotes From API
 async function getQuotes() {
   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
-    console.log(apiQuotes);
     newQuote();
   } catch (error) {
     // Catch Error Here
   }
 }
 
-// On Load
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.text} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 getQuotes();
 
-// newQuote();
